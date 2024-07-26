86a424e4a0e35afc15c2fc6eba608f45f6f14d3a
Brian Smiley <briantsmiley42@gmail.com>
2024-07-25 20:34:14 -0400

    adds task tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,8 @@
 import { useEffect } from "react";
 import useTaskStore from "./stores/useTaskStore";
-import { Task, TaskStatus } from "./interfaces/Task.d";
-import TaskItem from "./components/TaskItem";
+import TaskTabs from "./components/TaskTabs";
+import { dummyTasks } from "./assets/dummyTasks";
 
-const dummyTasks: Task[] = [
-  {
-    id: "1",
-    title: "Task 1",
-    description:
-      "This is a pending task description and has a much longer descriptionmuch longer descriptionmuch longer descriptionmuch longer descriptionmuch longer descriptionmuch longer descriptionmuch longer descriptionmuch longer descriptionmuch longer descriptionmuch longer descriptionmuch longer descriptionmuch longer description that goes on and on and on and on and on",
-    status: TaskStatus.Pending
-  },
-  {
-    id: "2",
-    title: "Task 2",
-    description: "This is an in-progress task description",
-    status: TaskStatus.InProgress
-  },
-  {
-    id: "3",
-    title: "Task 3",
-    description: "This is a completed task description",
-    status: TaskStatus.Completed
-  },
-  {
-    id: "4",
-    title: "Task 4",
-    description: "This is an archived task description",
-    status: TaskStatus.Archived
-  }
-];
 function App() {
   const { tasks, addTasks, clearTasks } = useTaskStore();
   useEffect(() => {
@@ -38,9 +11,10 @@ function App() {
   }, []);
   return (
     <div className="flex flex-col gap-2 p-2">
-      {tasks.map(task => (
+      {/* {tasks.map(task => (
         <TaskItem key={task.id} task={task} />
-      ))}
+      ))} */}
+      <TaskTabs tasks={tasks} />
     </div>
   );
 }
---
+++ src/components/TaskTabs.tsx
@@ -0,0 +1,64 @@
+import TaskList from "./TaskList";
+import { Task, TaskStatus } from "../interfaces/Task.d";
+
+export default function TaskTabs({ tasks }: { tasks: Task[] }) {
+  const pendingTasks = tasks.filter(task => task.status === TaskStatus.Pending);
+  const inProgressTasks = tasks.filter(
+    task => task.status === TaskStatus.InProgress
+  );
+  const completedTasks = tasks.filter(
+    task => task.status === TaskStatus.Completed
+  );
+  const archivedTasks = tasks.filter(
+    task => task.status === TaskStatus.Archived
+  );
+
+  return (
+    <div role="tablist" className="tabs tabs-bordered">
+      <input
+        type="radio"
+        name="task_tabs"
+        role="tab"
+        className="tab"
+        aria-label="Pending Tasks"
+        defaultChecked
+      />
+      <div role="tabpanel" className="tab-content p-10">
+        <TaskList tasks={pendingTasks} />
+      </div>
+
+      <input
+        type="radio"
+        name="task_tabs"
+        role="tab"
+        className="tab"
+        aria-label="In Progress Tasks"
+      />
+      <div role="tabpanel" className="tab-content p-10">
+        <TaskList tasks={inProgressTasks} />
+      </div>
+
+      <input
+        type="radio"
+        name="task_tabs"
+        role="tab"
+        className="tab"
+        aria-label="Completed Tasks"
+      />
+      <div role="tabpanel" className="tab-content p-10">
+        <TaskList tasks={completedTasks} />
+      </div>
+
+      <input
+        type="radio"
+        name="task_tabs"
+        role="tab"
+        className="tab"
+        aria-label="Archived Tasks"
+      />
+      <div role="tabpanel" className="tab-content p-10">
+        <TaskList tasks={archivedTasks} />
+      </div>
+    </div>
+  );
+}
