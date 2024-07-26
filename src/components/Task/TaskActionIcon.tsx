783d45c38fa995a53728ab1f00c64c2eaa5d3cf0
Brian Smiley <briantsmiley42@gmail.com>
2024-07-26 09:32:52 -0400

    icon changes

--- src/components/Task/TaskActionIcon.tsx
+++ src/components/Task/TaskActionIcon.tsx
@@ -1,20 +1,20 @@
 import { LucideIcon } from "lucide-react";
-import { Task } from "../../interfaces/Task";
 
 interface TaskActionIconProps {
-  task: Task;
   Icon: LucideIcon;
-  action: (id: Task["id"]) => void;
+  action: () => void;
+  tooltip?: string;
 }
 export default function TaskActionIcon({
-  task,
   Icon,
-  action
+  action,
+  tooltip
 }: TaskActionIconProps) {
   return (
     <button
-      className="flex items-center justify-center text-slate-500 hover:text-black hover:cursor-pointer rounded-md"
-      onClick={() => action(task.id)}
+      className="flex items-center justify-center text-slate-500 hover:text-black hover:cursor-pointer rounded-md tooltip"
+      onClick={action}
+      data-tip={tooltip ? tooltip : ""}
     >
       <Icon className="w-5 h-5 " strokeWidth={1.5} />
     </button>
--- src/components/Task/TaskActionIconGroup.tsx
+++ src/components/Task/TaskActionIconGroup.tsx
@@ -1,6 +1,7 @@
-import { Archive, Badge, BadgeCheck, Edit } from "lucide-react";
+import { Archive, ArchiveRestore, Badge, BadgeCheck, Edit } from "lucide-react";
 import { Task } from "../../interfaces/Task";
 import useTaskStore from "../../stores/useTaskStore";
+import TaskActionIcon from "./TaskActionIcon";
 
 interface TaskActionIconGroupProps {
   task: Task;
@@ -11,32 +12,34 @@ export default function TaskActionIconGroup({
   const { setTaskStatus } = useTaskStore();
   return (
     <div className="flex items-center gap-2">
-      <div onClick={() => setTaskStatus(task.id, "archived")}>
-        <Archive className="w-5 h-5 " strokeWidth={1.5} />
-      </div>
-
-      <div
-        className="flex items-center justify-center text-slate-500 hover:text-black hover:cursor-pointer rounded-md"
-        onClick={() => setTaskStatus(task.id, "completed")}
-      >
-        <Edit className="w-5 h-5 " strokeWidth={1.5} />
-      </div>
       {/* Complete/Incomplete Toggle */}
-      <div
-        className="flex items-center justify-center text-slate-500 hover:text-black hover:cursor-pointer rounded-md"
-        onClick={() =>
-          setTaskStatus(
-            task.id,
-            task.status === "completed" ? "pending" : "completed"
-          )
-        }
-      >
-        {task.status === "completed" ? (
-          <BadgeCheck className="w-5 h-5" strokeWidth={1.5} />
-        ) : (
-          <Badge className="w-5 h-5" strokeWidth={1.5} />
-        )}
-      </div>
+      {task.status === "completed" ? (
+        <TaskActionIcon
+          Icon={BadgeCheck}
+          action={() => setTaskStatus(task.id, "pending")}
+          tooltip="Uncomplete"
+        />
+      ) : (
+        <TaskActionIcon
+          Icon={Badge}
+          action={() => setTaskStatus(task.id, "completed")}
+          tooltip="Complete"
+        />
+      )}
+      <TaskActionIcon Icon={Edit} action={() => {}} tooltip="Edit" />
+      {task.status === "archived" ? (
+        <TaskActionIcon
+          Icon={ArchiveRestore}
+          action={() => setTaskStatus(task.id, "pending")}
+          tooltip="Unarchive"
+        />
+      ) : (
+        <TaskActionIcon
+          Icon={Archive}
+          action={() => setTaskStatus(task.id, "archived")}
+          tooltip="Archive"
+        />
+      )}
     </div>
   );
 }
