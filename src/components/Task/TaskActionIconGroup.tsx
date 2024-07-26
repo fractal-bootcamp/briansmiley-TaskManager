f8992148f2cfa5d1ce5d6c920d1226f8aeb38851
Brian Smiley <briantsmiley42@gmail.com>
2024-07-26 10:03:41 -0400

    adds progress handling

--- src/components/Task/TaskActionIconGroup.tsx
+++ src/components/Task/TaskActionIconGroup.tsx
@@ -1,4 +1,12 @@
-import { Archive, ArchiveRestore, Badge, BadgeCheck, Edit } from "lucide-react";
+import {
+  Archive,
+  ArchiveRestore,
+  Badge,
+  BadgeCheck,
+  CircleEllipsis,
+  CirclePlay,
+  Edit
+} from "lucide-react";
 import { Task } from "../../interfaces/Task";
 import useTaskStore from "../../stores/useTaskStore";
 import TaskActionIcon from "./TaskActionIcon";
@@ -24,9 +32,26 @@ export default function TaskActionIconGroup({
     tooltip: task.archived ? "Unarchive" : "Archive",
     action: () => setTaskArchived(task.id, !task.archived)
   };
+  const progess = {
+    icon: task.status === "inProgress" ? CircleEllipsis : CirclePlay,
+    tooltip: task.status === "inProgress" ? "Unbegin" : "Begin",
+    action: () =>
+      setTaskStatus(
+        task.id,
+        task.status === "inProgress" ? "pending" : "inProgress"
+      )
+  };
+  const showProgessStatus = !(task.status === "completed") && !task.archived;
   return (
     <div className="flex items-center gap-2">
       <TaskActionIcon Icon={Edit} action={() => {}} tooltip="Edit" />
+      {showProgessStatus && (
+        <TaskActionIcon
+          Icon={progess.icon}
+          action={progess.action}
+          tooltip={progess.tooltip}
+        />
+      )}
       <TaskActionIcon
         Icon={completion.icon}
         action={completion.action}
