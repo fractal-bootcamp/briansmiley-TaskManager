31f5c3f00ddf1249b2e957d17df3ec007e7a5a5d
Brian Smiley <briantsmiley42@gmail.com>
2024-07-27 10:54:47 -0400

    tweaks page height stuff to fix that

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import useTaskStore from "./stores/useTaskStore";
 import TaskTabs from "./components/TaskTabs";
-import { dummyTasks } from "./assets/dummyTasks";
+import { dummyTasks } from "./stores/dummyTasks";
 
 function App() {
   const { tasks, addTasks, clearTasks } = useTaskStore();
@@ -10,7 +10,7 @@ function App() {
     return () => clearTasks();
   }, []);
   return (
-    <div className="flex flex-col w-full h-full items-center bg-slate-200 gap-2 p-2">
+    <div className="flex flex-col w-full min-h-[100vh] items-center bg-slate-200 gap-2 p-2">
       <TaskTabs tasks={tasks} />
     </div>
   );
